28683749c15816979c977c5013f34b28de5518a7
NFuller721 <74070284+NFuller721@users.noreply.github.com>
2021-05-16 19:55:58 -0400

    Create Poll Form

--- react-frontend/src/Components/CreatePoll/CreatePollForm.js
+++ react-frontend/src/Components/CreatePoll/CreatePollForm.js
@@ -1,7 +1,11 @@
-import { Card, CardContent, Typography, Button } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
+// Components
 import ThemeProviderComponent from '../ThemeProviderComponent'
 
+// Material-ui
+import { Card, CardContent, Typography, Button, TextField } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+// CreatePollForm
 const CreatePollForm = () => {
   // Styles
   const useStyles = makeStyles({
@@ -20,7 +24,17 @@ const CreatePollForm = () => {
     <ThemeProviderComponent>
       <Card className={classes.Card}>
         <CardContent className={classes.CardContent}>
-          <Button variant="contained">Create Poll</Button>
+          <form>
+            <TextField label="Poll title" />
+
+            <TextField label="Leave blank to not include" placeholder="Option 1"/>
+            <TextField label="Leave blank to not include" placeholder="Option 2"/>
+            <TextField label="Leave blank to not include" placeholder="Option 3"/>
+            <TextField label="Leave blank to not include" placeholder="Option 4"/>
+            <TextField label="Leave blank to not include" placeholder="Option 5"/>
+
+            <Button variant="contained">Create Poll</Button>
+          </form>
         </CardContent>
       </Card>
     </ThemeProviderComponent>
