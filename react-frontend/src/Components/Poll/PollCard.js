c292214515dcff92a0496d0ce4987184db611134
NFuller721 <74070284+NFuller721@users.noreply.github.com>
2021-05-16 17:44:15 -0400

    POST request

--- react-frontend/src/Components/Poll/PollCard.js
+++ react-frontend/src/Components/Poll/PollCard.js
@@ -24,7 +24,7 @@ const PollCard = ({ pollId, pollInfo }) => {
   })
   const classes = useStyles()
 
-  const [voted, setVoted] = useState(true)
+  const [voted, setVoted] = useState(false)
 
   const onVote = () => {
     setVoted(!voted)
