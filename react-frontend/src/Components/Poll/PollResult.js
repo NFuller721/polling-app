92a7bbcd82a8e13697c65b7629a090399cfad3d0
NFuller721 <74070284+NFuller721@users.noreply.github.com>
2021-05-16 17:29:53 -0400

    POST request

--- react-frontend/src/Components/Poll/PollResult.js
+++ react-frontend/src/Components/Poll/PollResult.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PollResult = ({ value, max, label }) => {
   return (
-    <div className="PollResults">
+    <div className="PollResult">
       <label>{label}</label>
       <progress value={value} max={max}></progress>
     </div>
