677d62f9ee117fdfd3abd5bb04a7071f0ee41300
NFuller721 <74070284+NFuller721@users.noreply.github.com>
2021-05-16 19:51:21 -0400

    Create Poll Form

--- react-frontend/src/Components/CreatePoll/CreatePollForm.js
+++ react-frontend/src/Components/CreatePoll/CreatePollForm.js
@@ -1,11 +1,25 @@
 import { Card, CardContent, Typography, Button } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 import ThemeProviderComponent from '../ThemeProviderComponent'
 
 const CreatePollForm = () => {
+  // Styles
+  const useStyles = makeStyles({
+    Card: {
+      height: "450px",
+      width: "375px"
+    },
+    CardContent: {
+      boxSizing: "border-box",
+      height: "calc(100% - 16px)"
+    }
+  })
+  const classes = useStyles()
+
   return (
     <ThemeProviderComponent>
-      <Card>
-        <CardContent>
+      <Card className={classes.Card}>
+        <CardContent className={classes.CardContent}>
           <Button variant="contained">Create Poll</Button>
         </CardContent>
       </Card>
