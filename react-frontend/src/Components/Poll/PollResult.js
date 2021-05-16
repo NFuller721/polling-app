8cfdd67ba26b13cb7b63a9b3979a4a8610bd419e
NFuller721 <74070284+NFuller721@users.noreply.github.com>
2021-05-16 18:46:26 -0400

    POST request

--- react-frontend/src/Components/Poll/PollResult.js
+++ react-frontend/src/Components/Poll/PollResult.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {
+  Typography
+} from '@material-ui/core'
 
 const PollResult = ({ value, max, label }) => {
   return (
     <div className="PollResult">
-      <label>{label}</label>
+      <Typography>{label}</Typography>
       <progress value={value} max={max}></progress>
     </div>
   )
