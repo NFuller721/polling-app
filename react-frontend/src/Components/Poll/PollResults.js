8efe27ddfed2ed76405db53a6434ef8b0149b53e
NFuller721 <74070284+NFuller721@users.noreply.github.com>
2021-05-16 17:33:19 -0400

    POST request

--- react-frontend/src/Components/Poll/PollResults.js
+++ react-frontend/src/Components/Poll/PollResults.js
@@ -1,14 +1,39 @@
 import React from 'react'
 import PollResult from './PollResult'
+import { withRouter } from 'react-router-dom'
 
-const PollResults = () => {
-  return (
-    <div className="PollResults">
-      <PollResult value="1" max="4" label="Hello There"/>
-      <PollResult value="2" max="4" label="Hello There"/>
-      <PollResult value="1" max="4" label="Hello There"/>
-    </div>
-  )
+class PollResults extends React.Component {
+  constructor() {
+
+  }
+
+  async componentDidMount() {
+    const body = JSON.stringify(
+      {
+        pollId: this.props.match.params.pollId
+      }
+    )
+    const response = await fetch("/Api/467586970086574653/read", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body
+    })
+    const data = await response.json()
+
+    console.log(data)
+  }
+
+  render() {
+    return (
+      <div className="PollResults">
+        <PollResult value="1" max="4" label="Hello There"/>
+        <PollResult value="2" max="4" label="Hello There"/>
+        <PollResult value="1" max="4" label="Hello There"/>
+      </div>
+    )
+  }
 }
 
-export default PollResults
+export default withRouter(PollResults)
